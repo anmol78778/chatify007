5169769409a276328c763870e2bde9bc45be2baa
Anmol Kumar <anmolkroe@gmail.com>
2026-04-06 21:05:12 +0530

    chat container setup done

--- frontend/src/components/ChatsList.jsx
+++ frontend/src/components/ChatsList.jsx
@@ -25,7 +25,7 @@ const ChatsList = () => {
           <div className="flex items-center gap-3">
             <div className={`avatar ${onlineUsers.includes(chat._id) ? "online" : "offline"}`}>
               <div className="size-12 rounded-full">
-                <img src={chat.profilePic || "/avatar (4).png"} alt={chat.fullName} />
+                <img src={chat.profilePic || "/avatar(4).png"} alt={chat.fullName} />
               </div>
             </div>
             <h4 className="text-slate-200 font-medium truncate">{chat.fullName}</h4>
---
+++ frontend/src/components/MessageInput.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MessageInput = () => {
+  return (
+    <div>
+      sfsf
+    </div>
+  )
+}
+
+export default MessageInput
